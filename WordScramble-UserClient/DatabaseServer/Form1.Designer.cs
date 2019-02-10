fda8fe4d55134d9b461199baaa7722a35b80a56d
Ryan Goodman <ryan.goodman@aggiemail.usu.edu>
2019-02-09 23:06:39 -0700

    Added projects for each process. They are nested into the same solution for ease of tracking and work.

--- WordScramble-UserClient/DatabaseServer/Form1.Designer.cs
+++ WordScramble-UserClient/DatabaseServer/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WordScramble_UserClient
+﻿namespace DatabaseServer
 {
     partial class Form1
     {
@@ -28,17 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Word Scramble";
-            this.ResumeLayout(false);
-
+            this.Text = "Form1";
         }
 
         #endregion
